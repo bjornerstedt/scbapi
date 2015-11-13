f1a3785756d72dcfc88f9c17e56dccd59fdc65e8
bjornerstedt <jonas@bjornerstedt.org>
2015-11-13 16:10:59 +0100

    get_scb now has a minimal default interface.

--- R/get_scb.R
+++ R/get_scb.R
@@ -1,26 +1,33 @@
 # Download SCB data
+# Jonas Björnerstedt
 #
 # This function downloads queries saved as JSON files
 # returning a data_frame
 #
-# For more info, see
+# For more info, see:
 #
 #   http://www.github.com/bjornerstedt/scbapi
 #
 
-get_scb <- function(fname, download_data = TRUE, url = NA) {
+get_scb <- function(fname, download_data = TRUE, save_data = FALSE, url = NA) {
   library(httr)
   library(jsonlite)
   if(download_data) {
+    query <- fromJSON(readLines(str_c(fname, ".json")))
     if(is.na(url)) {
-      dataurl <- fromJSON(readLines(str_c(fname, ".json")))$url
-      if( is.null(dataurl) )
+      if( is.null(query$url) )
         stop("url has to be specified in json input")
     }
-    data <- content(POST(dataurl, body = readLines(str_c(fname, ".json")), encode = "json"), "text")
-    writeLines(data, str_c(fname, "_data.json"))
+    data <- content(POST(query$url, body = readLines(str_c(fname, ".json")), encode = "json"), "text")
+    if(save_data)
+      writeLines(data, str_c(fname, "_data.json"))
     scb <- fromJSON(data)
   } else
     scb <- fromJSON(readLines(str_c(fname, "_data.json")))
-  scb
+
+  indata <- scb$dataset$value
+  lab <- unlist(scb$dataset$dimension$Tid$category$label)
+  if(length(lab) != length(indata))
+    indata <- matrix(indata, nrow = length(lab))
+  data.frame(time = lab, x = indata )
 }
--- get_scb_test.R
+++ get_scb_test.R
@@ -3,20 +3,7 @@ library(dplyr)
 library(stringr)
 
 # Population
-scb <- get_scb("pop")
-indata <- scb$dataset$value
-lab <- as.numeric(unlist(scb$dataset$dimension$Tid$category$label))
-d<- matrix(indata, nrow = length(lab))
-pop <- data.frame(year = lab )
-
-pop$popwomen <- rowSums(d[ ,1:4 * 2])
-pop$popmen <- rowSums(d[ ,1:4 * 2 - 1])
+pop <- get_scb("pop")
 
 # Real GDP
-scb <- get_scb("gdpreal")
-indata <- scb$dataset$value
-lab <- unlist(scb$dataset$dimension$Tid$category$label)
-gdp <- data_frame(yq = lab, gdp=indata ) %>%
-    separate(yq, c("year", "quarter"), sep = "K", convert = TRUE) %>%
-    na.omit
-
+gdp <- get_scb("gdpreal")
